8a064fe10c7578f2a16f7ad506597f528ad9bc22
Keri Levesque <kerianne166@gmail.com>
2020-02-12 12:58:44 -0500

    set up signUp end point and http request

--- AnimalSpotter/Application/APIController.swift
+++ AnimalSpotter/Application/APIController.swift
@@ -19,10 +19,35 @@ class APIController {
     private let baseUrl = URL(string: "https://lambdaanimalspotter.vapor.cloud/api")!
     
     // create function for sign up
-    
+    func signUp(with user: User, completion: @escaping (Error?) -> ()) {
+        // create endpoint -specific URL
+        let signUpUrl = baseUrl.appendingPathComponent("users/signup")
+        // create URL request  from above
+        var request = URLRequest(url: signUpUrl)
+        // modify request for POST, add proper headers
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        // encode the user model to JSON, attach as request body
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            NSLog("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+        // set up data taskand handle response
+        URLSession.shared.dataTask(with: request) { (_, response, error) in
+            // handle response
+            
+        }.resume()
+    }
     // create function for sign in
     
     // create function for fetching all animal names
+    
+    // create function for fetching animal details
     
     // create function to fetch image
 }
